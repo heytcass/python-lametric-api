2bb4f0e2ebf6c69818d0f404c8fee1d95b6fc808
Tom Cassady <heytcass@gmail.com>
2016-10-19 00:06:13 -0400

    adding __init__.py

---
+++ pylametric/__init__.py
@@ -0,0 +1,13 @@
+import requests
+
+def send_notification(host, text, api_key):
+    enc_api = base64.b64encode(api_key.encode('ascii'))
+    not_url = 'http://' + host + ':8080/api/v2/device/notifications'
+    headers = {
+    'Content-type': 'application/json',
+    'Accept': 'application/json',
+    'Authorization': b'Basic ' + enc_api,
+    'Cache-Control': 'no-cache',
+     }
+    data = '{"priority": "warning", "icon_type": "none", "model": {"frames": [{$
+    requests.post(noturl, headers=headers, data=data)
